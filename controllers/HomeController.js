a379fbc3538d7c566bb3255e90842b79822af367
Lahiru Chanaka <kodikaragelahiru@gmail.com>
2024-11-16 23:53:31 +0530

    pos system codes

---
+++ controllers/HomeController.js
@@ -0,0 +1,12 @@
+function loadCounts() {
+  const customerCount = customerDatabase.length.toString();
+  $("#customerCount").text(customerCount);
+
+  const itemCount = itemDatabase.length.toString();
+  $("#itemCount").text(itemCount);
+
+  const orderCount = orderDatabase.length.toString();
+  $("#orderCount").text(orderCount);
+}
+
+loadCounts();
